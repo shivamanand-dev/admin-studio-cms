f5d5844c5a9c790ac3e5906798572fd6c405d1a2
shivamanand-dev <shivam_anand@outlook.com>
2023-02-18 17:46:27 +0530

    Theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+
+import store from "@/redux/store";
+import theme from "@/utils/Theme/theme";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
+  );
 }
