1670741363a61de1df33391fb8cd3ac909bc8e1b
shivamanand-dev <shivam_anand@outlook.com>
2023-02-22 21:55:24 +0530

    filename

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,6 +1,8 @@
 import Router from "next/router";
 import { BehaviorSubject } from "rxjs";
 
+import { app_routes } from "@/utils/constants/app_constants";
+
 import { fetchWrapper } from "./fetchWrapper";
 
 const userSubject = new BehaviorSubject(
@@ -20,40 +22,44 @@ export const userService = {
 };
 
 async function login(data) {
-  return await fetchWrapper.post("/auth/login", data).then(async (res) => {
-    if (res.success) {
-      await fetch("/api/login", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(res.authToken),
-      });
-
-      userSubject.next(res.userDetails);
-      localStorage.setItem("user", JSON.stringify(res.userDetails));
-    }
-    return res;
-  });
-}
-
-async function signup(data) {
-  return await fetchWrapper.post("/auth/signup", data).then(async (res) => {
-    if (res.success) {
+  return await fetchWrapper
+    .post(`/auth${app_routes.login}`, data)
+    .then(async (res) => {
       if (res.success) {
-        await fetch("/api/login", {
+        await fetch(`/api${app_routes.login}`, {
           method: "post",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(res.authToken),
         });
-        userSubject.next(res.userValue);
+
+        userSubject.next(res.userDetails);
         localStorage.setItem("user", JSON.stringify(res.userDetails));
       }
       return res;
-    }
-  });
+    });
+}
+
+async function signup(data) {
+  return await fetchWrapper
+    .post(`/auth${app_routes.signup}`, data)
+    .then(async (res) => {
+      if (res.success) {
+        if (res.success) {
+          await fetch(`/api${app_routes.login}`, {
+            method: "post",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(res.authToken),
+          });
+          userSubject.next(res.userValue);
+          localStorage.setItem("user", JSON.stringify(res.userDetails));
+        }
+        return res;
+      }
+    });
 }
 
 async function logout() {
