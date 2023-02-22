dafd4030f178a2fe2c6828db2f19bf3d125d7c47
shivamanand-dev <shivam_anand@outlook.com>
2023-02-22 22:34:01 +0530

    account

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import "@/styles/globals.css";
 
+import { Container, NoSsr } from "@mui/material";
 import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 
+import Navbar from "@/components/Navbar";
 import store from "@/redux/store";
 import { userService } from "@/services/user.services";
 import { ip_data_API } from "@/utils/constants/app_config";
@@ -19,7 +21,13 @@ export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <NoSsr>
+          <Navbar />
+          <div style={{ height: "64px" }}></div>
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </NoSsr>
       </ThemeProvider>
     </Provider>
   );
