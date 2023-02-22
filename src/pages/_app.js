ff23da23ad54c847a4d1be674558f7112edf7dab
shivamanand-dev <shivam_anand@outlook.com>
2023-02-22 22:57:53 +0530

    profile

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 
 import { Container, NoSsr } from "@mui/material";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
@@ -9,15 +10,25 @@ import Navbar from "@/components/Navbar";
 import store from "@/redux/store";
 import { userService } from "@/services/user.services";
 import { ip_data_API } from "@/utils/constants/app_config";
+import { app_routes, lockedRoutes } from "@/utils/constants/app_constants";
 import theme from "@/utils/Theme/theme";
 
 export default function App({ Component, pageProps }) {
   const user = userService.userValue;
+  const router = useRouter();
+
   useEffect(() => {
     if (user) {
       userService.updateUserCountry(ip_data_API);
     }
   });
+
+  useEffect(() => {
+    if (!user && lockedRoutes.includes(router.pathname)) {
+      router.push(app_routes.login);
+    }
+  });
+
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
